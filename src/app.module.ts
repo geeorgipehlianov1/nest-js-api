d89d78acef33da75daf7dca9ca605d6552a18fcb
Georgi Pehlivanov <george9040@abv.bg>
2024-04-01 15:01:22 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
