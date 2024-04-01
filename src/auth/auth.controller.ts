5ad79e408e20f1946a8a070a47fdb14236a3b322
Georgi Pehlivanov <george9040@abv.bg>
2024-04-01 16:14:24 +0300

    Add login and register functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  login() {
-    return this.authService.login();
+  login(@Body() loginDto: AuthDto) {
+    return this.authService.login(loginDto);
   }
 
   @Post('register')
-  register() {
-    return this.authService.register();
+  register(@Body() registerDto: AuthDto) {
+    return this.authService.register(registerDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  public login() {
-    return 'I logged in';
+  public async login({ email, password }: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+        hash: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    const isPasswordMatch = await argon.verify(user.hash, password);
+
+    if (!isPasswordMatch) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    return user;
   }
 
-  public register() {
-    return 'I am registered';
+  public async register({ email, password }: AuthDto) {
+    try {
+      const hash = await argon.hash(password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 }
